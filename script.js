2385933eb0b9d2124990f4e8a029af2bba453e0e
Nick De Raj <nickderaj@gmail.com>
2021-06-29 15:26:37 +0800

    Smooth scrolling added to nav bar

--- script.js
+++ script.js
@@ -43,3 +43,13 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
