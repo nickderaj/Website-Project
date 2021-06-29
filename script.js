28cad783ef5242ca4d4c2a608e6f4a13b784d7d4
Nick De Raj <nickderaj@gmail.com>
2021-06-29 15:38:36 +0800

    Adding dot functionality to sliders and cleaning up code

--- script.js
+++ script.js
@@ -168,12 +168,32 @@ const slider = function () {
   const maxSlide = slides.length;
 
   // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
   const goToSlide = function (slide) {
     slides.forEach(
       (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
     );
   };
 
+  // Next slide
   const nextSlide = function () {
     if (curSlide === maxSlide - 1) {
       curSlide = 0;
@@ -182,6 +202,7 @@ const slider = function () {
     }
 
     goToSlide(curSlide);
+    activateDot(curSlide);
   };
 
   const prevSlide = function () {
@@ -191,9 +212,17 @@ const slider = function () {
       curSlide--;
     }
     goToSlide(curSlide);
+    activateDot(curSlide);
   };
 
-  goToSlide(0);
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
   // Event handlers
   btnRight.addEventListener("click", nextSlide);
   btnLeft.addEventListener("click", prevSlide);
@@ -201,6 +230,14 @@ const slider = function () {
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") prevSlide();
     e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
   });
 };
 slider();
